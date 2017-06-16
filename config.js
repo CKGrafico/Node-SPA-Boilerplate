9af65ba8d37027f3acfb9ec4b93699ce187d90aa
Quique Fdez Guerra <efernandez@plainconcepts.com>
2017-06-16 15:34:12 +0200

    Improve configuration

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    port: 5000
+    port: process.env.PORT || 5000
 };
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+const glob = require('glob');
 const express = require('express');
 const config = require('./config');
 
+// Create server
 let app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// Configure bodyparser to access to data on POST
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT || config.port, function () {
-  console.log('Website running');
+// Require our routers
+let modules = glob.sync('./api/**/*.router.*');
+modules.forEach(_module => require(_module)(server));
+
+// Start the server
+app.listen(config.port, () => {
+  console.log(`${app.name} listening at ${config.port}`);
 });
